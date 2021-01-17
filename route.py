e4ee42b8b16ce81938dd6717f3fe3a4dfe7052e9
nvk518 <nvk5181@gmail.com>
2021-01-16 16:44:53 -0800

    fixed url routing

--- route.py
+++ route.py
@@ -14,11 +14,9 @@ tempdirectory = tempfile.gettempdir()
 model = tf.keras.models.load_model('SkinCD') 
 app = Flask(__name__) 
 
-  
-@app.route('/') 
-@app.route('/home')
-def home(): 
-    return render_template('upload.html') 
+@app.route('/')
+def home():
+    return render_template('upload.html')
   
 def finds(sfname): 
     img = image.load_img(sfname, target_size=(220, 220))
@@ -29,26 +27,28 @@ def finds(sfname):
     print(prediction) 
     return prediction
 
+@app.route('/predict', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        try:
+            f = request.files['file']
+            sfname = secure_filename(f.filename)
+            loc = os.path.join(tempdirectory, sfname)
+            f.save(loc)
+            if os.path.isfile(loc):
+                print ("File exist")
+            else:
+                print ("File not exist")
 
-  
-@app.route('/uploader', methods = ['GET', 'POST']) 
-def upload_file(): 
-    if request.method == 'POST': 
-        f = request.files['file']
-        sfname = secure_filename(f.filename)
-        loc = os.path.join(tempdirectory, sfname)
-        f.save(loc)
-        if os.path.isfile(loc):
-            print ("File exist")
-        else:
-            print ("File not exist")
-
-        val = finds(loc) 
-        if val < 0.5:
-            val = 'Benign'
-        else:
-            val = 'Malignant'
-        return render_template('predict.html', ss = val) 
+            val = finds(loc)
+            if val < 0.5:
+                val = 'Benign'
+            else:
+                val = 'Malignant'
+            return render_template('predict.html', ss = val)
+        except:
+            return render_template('upload.html')
+    return render_template('upload.html')
 
 if __name__ == '__main__': 
     app.run() 
